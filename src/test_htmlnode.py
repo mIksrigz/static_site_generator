798fb7cfef239228e7eba9d0f3350ff1042a6374
m.iksrigz <m.iksrigz@gmail.com>
2026-03-04 10:11:38 +0100

    converting Leaf node to html

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -50,6 +50,29 @@ class TestHTMLNode(unittest.TestCase):
                 node.__repr__(),
                 "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
                 )
+
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "hello, world!")
+        self.assertEqual(node.to_html(), "<p>hello, world!</p>")
+
+    def test_leaf_to_html_without_tag(self):
+        node = LeafNode(None, "hello, world!")
+        self.assertEqual(node.to_html(), "hello, world!")
+
+    def test_leaf_to_html_bold(self):
+        node = LeafNode("b", "hello, world!")
+        self.assertEqual(node.to_html(), "<b>hello, world!</b>")
+
+    def test_leaf_to_html_img_with_props(self):
+        node = LeafNode("img", "hello, world!", {"src": "/home/user/Pictures/test_image1.png", "alt": "test image"})
+        self.assertEqual(node.to_html(), '<img src="/home/user/Pictures/test_image1.png" alt="test image">hello, world!</img>')
+
+    def test_leaf_to_html_link_with_props(self):
+        node = LeafNode("a", "hello, world!", {"class": "greeting", "href": "https://boot.dev", "target": "_blank"})
+        self.assertEqual(node.to_html(), '<a class="greeting" href="https://boot.dev" target="_blank">hello, world!</a>')
+
+
 
 
 if __name__ == "__main__":

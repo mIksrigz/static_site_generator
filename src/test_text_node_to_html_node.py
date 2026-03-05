00a356e9675969dcb4229ffefd633c74b57b7658
m.iksrigz <m.iksrigz@gmail.com>
2026-03-05 08:37:35 +0100

    basic text node to html node convertion

---
+++ src/test_text_node_to_html_node.py
@@ -0,0 +1,17 @@
+import unittest
+
+from textnode import TextNode, TextType
+from text_node_to_html_node import text_node_to_html_node
+
+
+class TestTextToHtml(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+
+if __name__ == "__main__":
+    unittest.main()
+

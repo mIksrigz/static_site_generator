226d22a143bfb4663cdc2a0ce58254691c909cbf
m.iksrigz <m.iksrigz@gmail.com>
2026-03-05 09:17:11 +0100

    refactor

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -34,6 +34,38 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
                         "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
                         )
+
+
+# text node to html tests
+
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node")
+        self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
+
+
+    def test_image(self):
+        node = TextNode("This is a image node", TextType.IMAGE, "/home/user/pictures/test_image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "/home/user/pictures/test_image.png", "alt": node.text})
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -25,3 +26,27 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+
+def text_node_to_html_node(text_node):
+    text_type = text_node.text_type
+    text = text_node.text
+    url = text_node.url
+
+    match text_type:
+        case text_type.TEXT:
+            return LeafNode(None, text)
+        case text_type.BOLD:
+            return LeafNode("b", text)
+        case text_type.ITALIC:
+            return LeafNode("i", text)
+        case text_type.CODE:
+            return LeafNode("code", text)
+        case text_type.LINK:
+            return LeafNode("a", text, {"href": url})
+        case text_type.IMAGE:
+            return LeafNode("img", "", {"src": url, "alt": text})
+        case _:
+            raise Exception(f"{text_type} is not supported text type")
+
